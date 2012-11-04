b726f589588f6ebe193f78cb8f144af2ec08c64c
Shashank Sunkavalli <shashanksunkavalli@fas.harvard.edu>
2012-11-04 18:49:06 -0500

    Added Tunde's files

--- bloom/src/bloom/Main.java
+++ bloom/src/bloom/Main.java
@@ -1,13 +1,15 @@
 package bloom;
 
+import bloom.hash.HashFunction;
 import java.util.ArrayList;
-
-import bloom.filters.InvertibleBloomFilter;
 
 public class Main {
 
     public static void main(String[] args) {
-    	ArrayList a = new ArrayList();
+    	String name = "Tunde";
+    	int hash = HashFunction.MurmurHash(name.getBytes(), 0);
+    	System.out.println(hash);
+    	ArrayList<String> a = new ArrayList<String>();
     	a.add("tunde");
     	System.out.println(a.hashCode());
     	
