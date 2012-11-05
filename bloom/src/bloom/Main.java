b23011674ff0c60e7533c5e0699ec955ba352bd7
Tunde Agboola <tun.agboola@gmail.com>
2012-11-04 21:37:19 -0500

    Hash function completed for k=3

--- bloom/src/bloom/Main.java
+++ bloom/src/bloom/Main.java
@@ -1,17 +1,28 @@
 package bloom;
 
 import bloom.hash.HashFunction;
-import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) {
     	String name = "Tunde";
-    	int hash = HashFunction.MurmurHash(name.getBytes(), 0);
-    	System.out.println(hash);
-    	ArrayList<String> a = new ArrayList<String>();
-    	a.add("tunde");
-    	System.out.println(a.hashCode());
+    	System.out.println(name);
+    	int[] hashes = HashFunction.hash(name, 100);
+    	for(int ii=0; ii < hashes.length; ii++){
+    		System.out.println(hashes[ii]);
+    	}
+    	name = "Omar";
+    	System.out.println(name);
+    	hashes = HashFunction.hash(name, 100);
+    	for(int ii=0; ii < hashes.length; ii++){
+    		System.out.println(hashes[ii]);
+    	}
+    	name = "Shashank";
+    	System.out.println(name);
+    	hashes = HashFunction.hash(name, 100);
+    	for(int ii=0; ii < hashes.length; ii++){
+    		System.out.println(hashes[ii]);
+    	}
     	
     }
 }
